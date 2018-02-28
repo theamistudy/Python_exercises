535d9318997f5552938c4967c28e7280e702142d
theamistudy <the.amistudy@gmail.com>
2018-02-28 16:51:21 +0530

    work with list

---
+++ venv/Fundamentals/WorkWithList.py
@@ -0,0 +1,74 @@
+
+
+MyFirstList = [3,45,56,732]
+
+print (MyFirstList)
+print(type(MyFirstList))
+
+
+List2 = ["Hello",24,True, 34.5]
+print(List2)
+
+List3 = ["Hello World", List2]
+print(List3)
+
+
+#-------- range ------
+
+range(15)
+print(list(range(15)))
+
+x = list(range(8))
+print(x)
+
+z = list(range(100,400,25))
+print(z)
+
+
+
+#--------- Read from List -------------
+
+
+w  = ['a','b','c','d','e']
+print(w[1])
+print(len(w))
+print(w[-5])
+
+
+#------ Overwrite values --------
+
+w[2] = 78
+print(w)
+
+
+x = ['a','b','c','d','e']
+y= ['f','g','h','i','j']
+
+z = [x,y]
+print(z)
+print(z[-2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
