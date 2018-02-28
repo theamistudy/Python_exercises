16a907be345a956771b8da35315497ff81e4de10
theamistudy <the.amistudy@gmail.com>
2018-02-28 12:02:32 +0530

    First Commit

---
+++ venv/TypesOfVariable/ForLoop.py
@@ -0,0 +1,26 @@
+
+for i in range (5):
+    print("Hello Python")
+
+
+print(list(range(5)))
+
+
+
+for i in range (5):
+    print("Hello Python : ", i)
+
+
+
+#------ Another way -----
+
+
+myList = [10,100,1000]
+print(myList)
+
+for j in myList:
+    print("j is equal to :", j)
+
+
+
+
