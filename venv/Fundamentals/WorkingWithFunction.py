da8182c2eebdf491c63e6fdca056906fa8fb6c97
theamistudy <the.amistudy@gmail.com>
2018-02-28 17:16:25 +0530

    work with function

---
+++ venv/Fundamentals/WorkingWithFunction.py
@@ -0,0 +1,11 @@
+
+x = range(20,31)
+print(x)
+myList1 = list(x)
+print(myList1)
+print(len(myList1))
+print(type(myList1))
+print(max(myList1))
+print(min(myList1))
+print(max(2,45,67))
+print(min(34,56,2))
