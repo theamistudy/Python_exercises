27e0a8b9b88de10281071968789b7a25ddbbf30f
theamistudy <the.amistudy@gmail.com>
2018-02-28 11:01:13 +0530

    Working with Variables

---
+++ venv/TypesOfVariable/WorkWithVariable.py
@@ -0,0 +1,44 @@
+
+import math
+
+
+A = 10
+B = 5
+
+#---- Arithmetic ------
+
+C = A + B
+print(C)
+
+D = B / A
+print(D)
+
+
+#---- Squart root -------
+
+E = math.sqrt(A)
+print(E)
+print(round(E))
+
+
+#------- Work with String ------
+
+greeting = "Hello"
+name = "Bob"
+
+msg = greeting +" "+ name
+print(msg)
+
+
+#------ adding float ------
+
+F = 2.34
+G = 3.45
+
+H = F + G
+print(H)
+
+
+
+
+
