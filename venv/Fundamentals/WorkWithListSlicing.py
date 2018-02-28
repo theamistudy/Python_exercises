4d4c7f04b75a5707fd08f7a6c7ea555c34747099
theamistudy <the.amistudy@gmail.com>
2018-02-28 17:02:56 +0530

    work with list

---
+++ venv/Fundamentals/WorkWithListSlicing.py
@@ -0,0 +1,16 @@
+
+list1 = ['A','B','C','D','E','F','G','H','I','J']
+print(list1)
+
+print(list1[1:8])
+print(list1[1:])
+print(list1[:7])
+print(list1[2:7])
+print(list1[2:7:2])
+print(list1[-8:-3])
+
+print(list1[::3])
+
+print(list1[::-2])
+
+
