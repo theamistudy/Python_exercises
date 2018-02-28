54b425929b8c6c85b949c1f372b069975ca76963
theamistudy <the.amistudy@gmail.com>
2018-02-28 11:17:14 +0530

    Work with boolean

---
+++ venv/TypesOfVariable/WorkingWithBoolean.py
@@ -0,0 +1,33 @@
+
+#-------- Bollean / Logical ----------
+
+#True
+#False
+
+print(4 < 5)
+
+print (10 > 5)
+
+print (4 == 4)
+
+print ( 100 < 10)
+
+
+# ==
+# != <>
+
+result = 5 != 4
+
+print ("result1 :",result)
+print (type(result))
+
+
+result2 = not(5 > 4)
+print("Result2 :",result2)
+
+
+result3 = result or result2
+print(result3)
+
+result4 = result and result2
+print(result4)
