b1fde5cda6e1329fa94db3a9a499b2fab44f8c6c
theamistudy <the.amistudy@gmail.com>
2018-02-28 17:35:13 +0530

    work with packages

---
+++ venv/Fundamentals/WorkingWithPackages.py
@@ -0,0 +1,7 @@
+
+# install package as -- Search package in Git----- run command in terminal --   conda install packagename or pip install packagename
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+#-- Intentionally keep blank....objective is to add package...
