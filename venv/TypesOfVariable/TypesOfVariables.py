139830b413ebfa3df5176a5b4b1c44d84e3fcdda
theamistudy <the.amistudy@gmail.com>
2018-02-28 10:45:26 +0530

    Type of variables

---
+++ venv/TypesOfVariable/TypesOfVariables.py
@@ -0,0 +1,32 @@
+# ----- integer ------
+
+x = 2
+print(type(x))
+print(x)
+
+# ----------- float -----
+
+
+y = 2.5
+print(y)
+print(type(y))
+
+
+#---- String -----
+
+a = "hello"
+print(a)
+
+b = 'hello2'
+print(b)
+
+c = '2'
+print(c)
+
+
+#------ Logical data type ------
+
+q1 = True
+print(q1)
+print(type(q1))
+
